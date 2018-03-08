203e8cd7832f6cf8e1989cb0c8343a60e44409e3
Patrick Sletvold <patricksletvold@hotmail.com>
2018-03-08 20:59:11 +0100

    Convert buildFiles to use execa instead on npm-run-script
    So much nicer

--- buildFiles.js
+++ buildFiles.js
@@ -1,4 +1,4 @@
-const npmRunScript = require("npm-run-script");
+const execa = require("execa");
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -11,20 +11,9 @@ module.exports = async function buildFiles(modPath) {
   try{
     await copyConfig(modPath)
 
-    await new Promise((resolve, reject) => {
-        const child = npmRunScript('(cd ' + modPath + ' && brunch b -p)');
-        child.once('error', (error) => {
-            //console.trace(error);
-            //process.exit(1);
-            reject(error)
-        });
-        child.once('exit', (exitCode) => {
-            //console.log('Brunch exited with code ', exitCode);
-            //process.exit(exitCode);
-            console.log('Mod files built, exited with code ' + exitCode)
-            resolve(exitCode)
-        });
-    })
+    const result = await execa('brunch', ['b','-p'],{ cwd: modPath })
+    console.log(result.stdout)
+    console.log('Mod files built, exited with code ' + result.code)
   }
   catch(err){
     console.error(err);
